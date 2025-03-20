73b89016e402d5e8f1555ac7ca620e3558aacf6c
huibertt <dennis87767@gmail.com>
2025-03-20 09:49:27 +0100

    Update punishments.js

--- punishments.js
+++ punishments.js
@@ -11,4 +11,5 @@ export const punishmentCategories = [
     "Categorie 8: Permanente ban",
     "Categorie 9: Account-wipe",
     "Categorie 10: Geen unban mogelijk (GUM) + discord ban"
+    "Categorie 11: Gangstrike"
 ];
